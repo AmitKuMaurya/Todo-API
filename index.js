80af87cc1e40b257ec9df8deda940a4b376e295f
AmitKuMaurya <amit69maurya69@gmail.com>
2022-12-13 11:17:46 +0530

    Done again

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = require("express")
+const {connection} = require("./config/db");
 const cors = require("cors")
+const app = express();
 require("dotenv").config();
+
 const {userController} = require("./routes/user.route")
 const {todosController} = require("./routes/todos.route")
-const {connection} = require("./config/db")
 const {authentication} = require("./middlewares/authentification")
 
-const app = express();
 let PORT =  process.env.PORT_NO || 7080 ;
 
+app.use(cors())
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Home page");
 })
 
-app.use(cors())
 
 app.use("/user", userController)
 app.use(authentication)
@@ -25,10 +26,9 @@ app.listen(PORT, async () => {
     try{
         await connection;
         console.log("My DataBase is Online...")
-    }
-    catch(err){
+    } catch(err){
         console.log("Error connnecting to DB")
         console.log(err)
     }
-    console.log(`listening on PORT 8080`)
+    console.log(`listening on port ${PORT}`)
 })
